433f131ba4c25d33925952245ebdc4a36a6ef3a7
João Pedro <118128987+JoaoPedro0116@users.noreply.github.com>
2023-08-09 11:40:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const key = "b090f2be2e87f981573c8f468e8a06fnb"
+
+
+
+async function buscarcidade(cidade){
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`).then(resposta => resposta.json())
+
+    console.log(dados)
+   
+}
+
+function cliqueinoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+    
+    buscarcidade(cidade)
+}
