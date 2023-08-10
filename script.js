3f687f5ee6291199e02ae3cc08b1500dd71dba9e
João Pedro <118128987+JoaoPedro0116@users.noreply.github.com>
2023-08-10 08:42:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const key = "b090f2be2e87f981573c8f468e8a06fnb"
 
 
 
-async function buscarcidade(cidade){
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`).then(resposta => resposta.json())
+// async function buscarcidade(cidade){
+//     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`).then(resposta => resposta.json())
 
-    console.log(dados)
+//     console.log(dados)
    
-}
+// }
 
 function cliqueinoBotao() {
     const cidade = document.querySelector(".input-cidade").value
