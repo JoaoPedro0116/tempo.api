eabb7844fdcd381ad9dfe62ecc3549dcfc08aff9
João Pedro <118128987+JoaoPedro0116@users.noreply.github.com>
2023-08-12 17:47:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const key = "b090f2be2e87f981573c8f468e8a06fb"
+// chave
 
 // function cliqueinoBotao() {
 //     const cidade = document.querySelector(".input-cidade").value
@@ -14,16 +15,17 @@ const key = "b090f2be2e87f981573c8f468e8a06fb"
 
 
 
-
+// coletando dados do input para submeter ao api
 function colocarNaTela(dados){
     console.log(dados)
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
     document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
     document.querySelector(".descricao").innerHTML = dados.weather[0].description
     document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
-    document.querySelector(".tempomin").innerHTML = "tempo min " + Math.floor(dados.main.temp_min) +  "°C"
+    document.querySelector(".umidade").innerHTML =  "umidade " + Math.floor(dados.main.humidity) + "%"
 }
 
+// api convertendo em json
 async function buscarCidade(cidade){
     let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
     cidade + 
@@ -43,3 +45,13 @@ function cliqueiNoBotao(){
 
    buscarCidade(cidade)
 }
+// funcao para verificar a tecla enter e servir o button submit
+document.addEventListener("keypress", function(e) {
+    if(e.key === 'Enter') {
+    
+        var btn = document.querySelector("#btn-buscar");
+      
+      btn.click();
+    
+    }
+  });
